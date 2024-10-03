1d76381975a9417eddf58d44bb97163bae7221e4
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-10-03 10:10:18 +0530

    detected left click

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import keyboard
 import pyautogui
+from pynput.mouse import Listener
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        if button.name == 'left':
+            print(f"Left mouse button clicked at ({x}, {y})")
 
 def clickFunction(r: int):
     pyautogui.click(button="left", clicks=r, interval=0.01)
@@ -8,4 +14,5 @@ if __name__ == '__main__':
     # activation -> ctrl + shift + i
     keyboard.wait("ctrl+alt+m")
     print("activation successfull")
-    clickFunction(10^1000)
+    with Listener(on_click=on_click) as listener:
+        listener.join()
