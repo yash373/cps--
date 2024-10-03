3fead05e4b4fbfbf786be4b8aa65f4619a73c6c5
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-10-03 09:49:40 +0530

    tested cps

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import keyboard
 import pyautogui
 
 def clickFunction(r: int):
-    for i in range(r):
-        pyautogui.leftClick()
+    pyautogui.click(button="left", clicks=r, interval=0.01)
 
 if __name__ == '__main__':
     # activation -> ctrl + shift + i
-    keyboard.wait("ctrl+shift+i")
+    keyboard.wait("ctrl+alt+m")
     print("activation successfull")
+    clickFunction(10^1000)
